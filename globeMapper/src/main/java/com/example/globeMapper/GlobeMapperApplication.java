f04e22b405060c771886750502f76e130d768744
u759 <aydennguyen604@gmail.com>
2025-01-18 14:42:20 -0800

    start backend

---
+++ globeMapper/src/main/java/com/example/globeMapper/GlobeMapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.globeMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobeMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobeMapperApplication.class, args);
+	}
+
+}
---
+++ globeMapper/src/test/java/com/example/globeMapper/GlobeMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globeMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobeMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
