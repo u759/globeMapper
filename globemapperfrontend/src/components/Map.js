5efff2bbce89c7fec4c9c2a8abca5aad9f3e5a76
BorisZhang12 <zhangboris12@gmail.com>
2025-01-19 02:42:35 -0800

    Fixed zoom to restrict to one world view

--- globemapperfrontend/src/components/Map.js
+++ globemapperfrontend/src/components/Map.js
@@ -81,10 +81,15 @@ function Map() {
         zoomControl={false}
         maxBounds={maxBounds}
         maxBoundsViscosity={1.0}
+        minZoom={2}
+        maxZoom={6}
+        doubleClickZoom={false}
+        bounceAtZoomLimits={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap={true}
         />
         <ZoomControl position="bottomright" />
         <LocationMarkers />
