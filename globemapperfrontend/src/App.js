5884dd408078ab553aa53bd099c593a025388d98
Lo3wer <leozhang226@gmail.com>
2025-01-18 16:01:55 -0800

    added basic map

--- globemapperfrontend/src/App.js
+++ globemapperfrontend/src/App.js
@@ -1,23 +1,43 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Sample locations - replace with your actual data
+  const locations = [
+    {
+      position: [51.505, -0.09],
+      name: "London",
+      description: "Capital of England"
+    },
+    {
+      position: [40.7128, -74.0060],
+      name: "New York",
+      description: "The Big Apple"
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer 
+        center={[51.505, -0.09]} 
+        zoom={13} 
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {locations.map((location, index) => (
+          <Marker key={index} position={location.position}>
+            <Popup>
+              <h3>{location.name}</h3>
+              <p>{location.description}</p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
